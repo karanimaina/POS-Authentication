fa960530baed2f644ae6fc1ffda95271d225bd87
Felix Karani Maina <fkarani@hq.ekenya.co.ke>
2023-08-24 19:15:15 +0300

    add base entity attruibutes

--- src/main/java/com/pos_authentication/model/BaseEntity.java
+++ src/main/java/com/pos_authentication/model/BaseEntity.java
@@ -1,4 +1,33 @@
 package com.pos_authentication.model;
 
-public class BaseEntity {
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.jmx.export.annotation.ManagedNotifications;
+
+@NoArgsConstructor
+@MappedSuperclass
+@Getter
+@Setter
+@AllArgsConstructor
+public abstract class BaseEntity {
+    @Id
+    private long id;
+    @CreatedBy
+    protected  String createdBy;
+    @CreatedDate
+    protected String createdOn;
+    @LastModifiedBy
+    protected  String lastModifiedBy;
+    @LastModifiedDate
+    protected String lastModifiedDate;
+    private  boolean softDelete;
+
 }
