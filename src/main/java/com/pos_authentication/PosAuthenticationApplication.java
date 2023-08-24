0520a36defb88521015ec052a43840a48d7bd282
Felix Karani Maina <fkarani@hq.ekenya.co.ke>
2023-08-24 19:09:42 +0300

    first commit

---
+++ src/main/java/com/pos_authentication/PosAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.pos_authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PosAuthenticationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pos_authentication/PosAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pos_authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
